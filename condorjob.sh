bb1c86894013d1d54a7cd22e1a2b8bbe09a9a253
ruben <r@g.it>
2023-07-15 15:36:04 +0200

    condor support updated

--- condorjob.sh
+++ condorjob.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 /afs/cern.ch/work/r/rgargiul/code_reco/step3_fromttree.py /afs/cern.ch/work/r/rgargiul/crilin_input/$3.root /afs/cern.ch/work/r/rgargiul/reco_data/$3/tree_and_json/$3_out_$2.root $4 --frontboard $5 --maxevents $6 --offset $(($2*$6))
+python3 /afs/cern.ch/work/r/rgargiul/code_reco/step3_fromttree.py /afs/cern.ch/work/r/rgargiul/crilin_input/$3.root /afs/cern.ch/work/r/rgargiul/reco_data/$3/tree_and_json/$3_out_$2.root $4 $5 --maxevents $6 --offset $(($2*$6))
