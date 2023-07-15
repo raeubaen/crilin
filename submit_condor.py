bbf8b87dfca7afb96a1af8ce259d138ef53a3342
ruben <r@g.it>
2023-07-15 16:13:11 +0200

    generalized input output condor folder stuff

--- submit_condor.py
+++ submit_condor.py
@@ -9,27 +9,28 @@ parser.add_argument('label', type=str, help='label', default="")
 parser.add_argument('fb', type=int, help='Front board (0/1)', default=0)
 parser.add_argument('nevents_per_job', type=int, help='nevents_per_job', default=200)
 parser.add_argument('njobs', type=int, help='njobs', default=500)
-parser.add_argument('--mainfolder', type=str, help="Folder where crilin_input, the repo directory and reco_data are placed", default='/afs/cern.ch/work/r/rgargiul/')
-parser.add_argument('--repofolder', type=str, help="Name of the folder with the repo", default='code_reco')
+parser.add_argument('--rootinputfolder', type=str, help="Root input folder", default='/eos/user/r/rgargiul/www/crilin/input/')
+parser.add_argument('--condorfolder', type=str, help="Folder where logs etc. are saved", default='/afs/cern.ch/work/r/rgargiul/crilin_jobs')
+parser.add_argument('--rootoutfolder', type=str, help="Root/json out folder", default='/eos/user/r/rgargiul/www/crilin/output/')
 
 args = parser.parse_args()
 v = vars(args)
 print(v)
 vars().update(v)
 
-os.system(f"mkdir ../reco_data/{nrun}")
-os.system(f"mkdir ../reco_data/{nrun}/output")
-os.system(f"mkdir ../reco_data/{nrun}/error")
-os.system(f"mkdir ../reco_data/{nrun}/log")
-os.system(f"mkdir ../reco_data/{nrun}/tree_and_json")
+os.system(f"mkdir {condorfolder}/{nrun}")
+os.system(f"mkdir {condorfolder}/{nrun}/output")
+os.system(f"mkdir {condorfolder}/{nrun}/error")
+os.system(f"mkdir {condorfolder}/{nrun}/log")
+os.system(f"mkdir {rootoutfolder}/{nrun}")
 
 sub = open("jobsub.condor", "r").read()
 
 for key in v:
   sub = sub.replace(f"${key}", str(v[key]))
 
-f = open(f"../reco_data/{nrun}/jobsub{nrun}.condor", "w")
+f = open(f"{condorfolder}/{nrun}/jobsub{nrun}.condor", "w")
 f.write(sub)
 f.close()
 
-os.system(f"condor_submit ../reco_data/{nrun}/jobsub{nrun}.condor")
+os.system(f"condor_submit {condorfolder}/{nrun}/jobsub{nrun}.condor")
